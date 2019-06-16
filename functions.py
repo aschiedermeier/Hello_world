ca40699d9a1963e85bfb27686989908a11d2f71a
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-06-16 20:01:45 +0200

    played with functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def greeting(name):
+    '''
+    ### print a greeting
+    '''
+    print ("Hi "+str(name))
+
+def calculate(a):
+    b = a
+    c = a * a
+    return b,c
+    
--- main_code.py
+++ main_code.py
@@ -2,3 +2,8 @@ print("hello")
 with open('data_files/pi_digits.txt') as file_object:
     contents = file_object.read()
     print(contents)
+
+
+import functions
+functions.greeting("Andi")
+print(functions.calculate(5))
