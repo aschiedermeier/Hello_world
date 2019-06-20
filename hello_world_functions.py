1ac2f2f4f4b8c5f8745ac1176e23213dd5a92e6c
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-06-20 22:02:28 +0200

    rename

--- hello_world_functions.py
+++ hello_world_functions.py
@@ -1,9 +1,15 @@
+################################
+# extra file for functions to play around with
+################################
+
+# greeting
 def greeting(name):
     '''
     ### print a greeting
     '''
     print ("Hi "+str(name))
 
+# calculate
 def calculate(a):
     b = a
     c = a * a
