d41ddc144d4302be960b6ab3d83f2a3845cac02a
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-08-18 00:18:31 +0200

    recall method

--- Starsigns/Starsigns.py
+++ Starsigns/Starsigns.py
@@ -116,70 +116,57 @@ class Starsign ():
     def set_modality(self,modality):
         '''set the modality of a starsign'''
         self.modality = modality 
-""" 
-    def recall_month(self,rounds=5, recall=3):
+
+    def recall_month(self):
         ''' recall month of starsign '''
-"""
-    
+        print ("When is the birthday of " + self.name + " ?")
+        print (self.month)
+        ans = input().lower()
+        ans = ans[0:3]
+        if ans == self.month[0:3]:
+            print ("Yes, it's " + self.month)
+        else:
+            print ("No, it's " + self.month)
+
+# define 12 starsign objects
 aries = Starsign("Aries","april")
-print (aries.get_descriptive_name())
 aries.set_modality(cardinal)
 aries.set_element(fire)
-print (aries.get_descriptive_name())
-print(aries.modality.power)
-print(aries.element.aspect)
-
-
 taurus = Starsign("Taurus","May")
 taurus.set_modality(fixed)
 taurus.set_element(earth)
-print (taurus.get_descriptive_name())
-print(taurus.modality.power)
-print(taurus.element.aspect)
-
 gemini = Starsign("Gemini","June")
 gemini.set_modality(mutable)
 gemini.set_element(air)
-print (gemini.get_descriptive_name())
-print(gemini.modality.power)
-print(gemini.element.aspect)
-
 cancer = Starsign("Cancer","July")
 cancer.set_modality(cardinal)
 cancer.set_element(water)
-
 leo = Starsign("Leo","August")
 leo.set_modality(fixed)
 leo.set_element(fire)
-
 virgo = Starsign("Virgo","September")
 virgo.set_modality(mutable)
 virgo.set_element(earth)
-
 libra = Starsign("Libra","October")
 libra.set_modality(cardinal)
 libra.set_element(air)
-
 scorpio = Starsign("Scorpio","November")
 scorpio.set_modality(fixed)
 scorpio.set_element(water)
-
 sagittarius = Starsign("Sagittarius","December")
 sagittarius.set_modality(mutable)
 sagittarius.set_element(fire)
-
 capricorn = Starsign("Capricorn","January")
 capricorn.set_modality(cardinal)
 capricorn.set_element(earth)
-
 aquarius = Starsign("Aquarius","February")
 aquarius.set_modality(fixed)
 aquarius.set_element(air)
-
 pisces = Starsign("Pisces","March")
 pisces.set_modality(mutable)
 pisces.set_element(water)
 
+# list 12 signs
 StarsignList = [aries,taurus,gemini,cancer,leo,virgo,libra,scorpio,sagittarius,capricorn,aquarius,pisces]
 print (len(StarsignList))
 for i in StarsignList:
@@ -189,10 +176,11 @@ for i in StarsignList:
     print()
 
 
-# 
 
-# Recall month of Starsign
-import random as rn
+"""
+
+
+# recalling month of starsigns
 
 # ask how many starsigns to recall 
 entered = False
@@ -225,18 +213,22 @@ while entered == False:
         print ("Error: the value is not within permitted range (1-10)")
     else: 
         entered = True
+"""
 
+recall = 1
+rounds = 1
 
+# Recall month of Starsign using recall method
+print("\n!!!recalling method!!!\n")
+#aries.recall_month()
+
+import random as rn
 for r in range(rounds):
     i = rn.randint(0,recall-1) # recall how many starsigns
-    print ("When is the birthday of " + StarsignList[i].name + " ?")
-    print (StarsignList[i].month)
-    ans = input().lower()
-    ans = ans[0:3]
-    if ans == StarsignList[i].month[0:3]:
-        print ("Yes, it's " + StarsignList[i].month)
-    else:
-        print ("No, it's " + StarsignList[i].month)
+    print (r)
+    print (i)
+    StarsignList[i].recall_month()
+
 
 
 # recall elements and modality
